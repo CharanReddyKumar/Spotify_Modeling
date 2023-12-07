83fae264c571523c2ad30474f9321755ab2f82a9
parameshwarbhat411 <parameshwar.bhat@gwmail.gwu.edu>
2023-12-07 17:11:38 -0500

    feature select

--- danceability_prediction.py
+++ danceability_prediction.py
@@ -50,3 +50,16 @@ from sklearn.model_selection import GridSearchCV
 # Selecting relevant features and the target variable
 features = ['energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
 target = 'danceability'
+
+# Handling missing values
+spotify_data = df_danceabiltiy.dropna(subset=features + [target])
+
+# Splitting the dataset into training and testing sets
+X = spotify_data[features]
+y = spotify_data[target]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Normalizing the feature data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
